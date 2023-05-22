842c62ec24ae7a3e8bf26915c6d88e5fa0a7b586
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-05-22 14:22:27 +0100

    completed first 3 sections of fundamentals

---
+++ functions.js
@@ -0,0 +1,13 @@
+/* 
+Modify the previous function sayHello so it accepts one argument, and calling sayHello('Sarah') displays a personalised message:
+
+> sayHello('Sarah');
+'Hello Sarah'
+Bonus: can you find another way to write the sayHello function, without using the sign + to concatenate the strings?
+
+> sayHello();
+'Hello'
+
+*/
+
+
